20cc7ab7f023479525b05250c9f3e6aa1a94cd16
Rubich Viktor <syzspectroom@gmail.com>
2014-11-20 18:32:06 +0200

    routes i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   get 'publications/index'
 
-  root to: 'main#index'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    root to: 'main#index'
     resources :news, only: [:index, :show]
     resources :publications, only: [:index]
-  end
+  end    
+   root to: redirect("/#{I18n.locale}", status: 302), as: :redirected_root
 end
