31b862247ba227e45fd323c6751d4f6a8f59cbbd
Rubich Viktor <syzspectroom@gmail.com>
2016-03-10 13:43:16 +0200

    fixes for form

--- app/controllers/advert_forms_controller.rb
+++ app/controllers/advert_forms_controller.rb
@@ -1,9 +1,9 @@
 class AdvertFormsController < ApplicationController
   def create
     @advert_form = AdvertForm.new(advert_form_params)
+    @back_url = params.fetch(:back_url,'').gsub(/http:/, '')
     if @advert_form.save
       RequestMailer.request_mail(@advert_form).deliver
-      @back_url = params[:back_url][5..-1]
       render 'success'
     end
 
@@ -13,6 +13,6 @@ class AdvertFormsController < ApplicationController
     def advert_form_params
       params.require(:advert_form).permit(
       :company_title, :address, :advice, :layout, :name,
-      :phone, :email, :photo, :logo)
+      :phone, :email, :photo, :photo_cache, :logo, :logo_cache)
     end
 end
